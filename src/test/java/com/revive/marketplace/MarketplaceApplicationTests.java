94f88fdff04acf7fcca5c69cf77e515e371b94f3
NepyAnna <nepyann@gmail.com>
2025-03-18 18:38:23 +0100

    first commit

---
+++ src/test/java/com/revive/marketplace/MarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revive.marketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
