72e3117cf24ca7023ee54be0dee22bcb9a030f4d
uxoa <palomita@gmail.com>
2025-03-18 20:15:32 +0100

    Fix: auth WebSecurityConfig roles

--- src/main/java/com/revive/marketplace/auth/CustomUserDetailsService.java
+++ src/main/java/com/revive/marketplace/auth/CustomUserDetailsService.java
@@ -20,25 +20,19 @@ public class CustomUserDetailsService implements UserDetailsService {
     
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // Buscar el usuario por su nombre de usuario
         Optional<User> userOptional = userRepository.findByUsername(username);
-        
-        // Si no se encuentra el usuario, lanzar una excepción
         if (userOptional.isEmpty()) {
             throw new UsernameNotFoundException("User Not Found with username: " + username);
         }
         
         User user = userOptional.get();
-        
-        // Convertir el rol del usuario a una autoridad para Spring Security
-        // Si el rol es USER, se crea una autoridad ROLE_USER, y si es ADMIN, ROLE_ADMIN
         SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + user.getRole().name());
         
-        // Devolver el objeto UserDetails con la información del usuario
+
         return new org.springframework.security.core.userdetails.User(
               user.getUsername(),
               user.getPassword(),
-              // Como solo tienes un rol, no es necesario un stream, solo pasamos la autoridad
+           
               java.util.Collections.singletonList(authority)
         );
     }
